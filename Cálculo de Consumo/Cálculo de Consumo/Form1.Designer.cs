b06b1dbd91e47a6aee341fec73f0b893f8e1f6c9
Victor SS <142058707+Victor-SS@users.noreply.github.com>
2024-05-15 21:32:18 -0300

    Add files via upload

---
+++ Cálculo de Consumo/Cálculo de Consumo/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Cálculo_de_Consumo
+{
+    partial class FrmConsumo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblVeiculo = new System.Windows.Forms.Label();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblLitros = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtLitros = new System.Windows.Forms.TextBox();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.txtVeiculo = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(46, 34);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblVeiculo
+            // 
+            this.lblVeiculo.AutoSize = true;
+            this.lblVeiculo.Location = new System.Drawing.Point(46, 63);
+            this.lblVeiculo.Name = "lblVeiculo";
+            this.lblVeiculo.Size = new System.Drawing.Size(44, 13);
+            this.lblVeiculo.TabIndex = 1;
+            this.lblVeiculo.Text = "Veículo";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(46, 93);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(51, 13);
+            this.lblDistancia.TabIndex = 2;
+            this.lblDistancia.Text = "Distância";
+            // 
+            // lblLitros
+            // 
+            this.lblLitros.AutoSize = true;
+            this.lblLitros.Location = new System.Drawing.Point(46, 131);
+            this.lblLitros.Name = "lblLitros";
+            this.lblLitros.Size = new System.Drawing.Size(92, 13);
+            this.lblLitros.TabIndex = 3;
+            this.lblLitros.Text = "Litros Consumidos";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(144, 31);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(260, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtLitros
+            // 
+            this.txtLitros.Location = new System.Drawing.Point(144, 131);
+            this.txtLitros.Name = "txtLitros";
+            this.txtLitros.Size = new System.Drawing.Size(104, 20);
+            this.txtLitros.TabIndex = 3;
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(144, 93);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(104, 20);
+            this.txtDistancia.TabIndex = 2;
+            // 
+            // txtVeiculo
+            // 
+            this.txtVeiculo.Location = new System.Drawing.Point(144, 63);
+            this.txtVeiculo.Name = "txtVeiculo";
+            this.txtVeiculo.Size = new System.Drawing.Size(260, 20);
+            this.txtVeiculo.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(49, 180);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(127, 52);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar Campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(277, 180);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(127, 52);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Cálcular Consumo";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmConsumo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 260);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtVeiculo);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.txtLitros);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblLitros);
+            this.Controls.Add(this.lblDistancia);
+            this.Controls.Add(this.lblVeiculo);
+            this.Controls.Add(this.lblNome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmConsumo";
+            this.Text = "Cálculo de Consumo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblVeiculo;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblLitros;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtLitros;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.TextBox txtVeiculo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Cálculo de Consumo/Cálculo de Consumo/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_de_Consumo
+{
+    public partial class FrmConsumo : Form
+    {
+        public FrmConsumo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtVeiculo.Clear();
+            txtDistancia.Clear();
+            txtLitros.Clear();
+            txtNome.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string nome;
+            string veiculo;
+            string mensagem;
+            decimal distancia;
+            decimal litros;
+            decimal consumo;
+
+            mensagem = ValidarPreenchimento();
+            if(mensagem != string.Empty)
+            {
+                MessageBox.Show(mensagem, "Cálculo de Consumo",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            nome = txtNome.Text;
+            veiculo = txtVeiculo.Text;
+            distancia = decimal.Parse(txtDistancia.Text);
+            litros = decimal.Parse(txtLitros.Text);
+
+            consumo = CalcularConsumo(distancia, litros);
+            mensagem = RetornarMensagem(consumo, nome, veiculo);
+
+            MessageBox.Show(mensagem, "Cálculo de Consumo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private decimal CalcularConsumo(decimal dist, decimal lit)
+        {
+            decimal result = dist / lit;
+            return result;
+        }
+        private string RetornarMensagem(decimal consumo, string nm, string vc)
+        {
+            string msg = nm + ", seu veículo " + vc +
+                " faz " + consumo.ToString("#0.0") + " km/l\n"
+                + "Ele é ";
+
+            if (consumo < 12)
+            {
+                msg += "Gastão!";           // > 8     
+            }
+            else if (consumo >= 15)
+            {
+                msg += "Econômico!";        // < 7
+            }
+            else
+            {
+                msg += "Aceitavel!";        // >= 7
+            }
+            return msg;
+        }
+        private string ValidarPreenchimento()
+        {
+            string msgErro = string.Empty;
+            if (txtNome.Text == string.Empty)
+            {
+                msgErro += "Campo Nome em branco.\n";
+            }
+            if (txtVeiculo.Text == string.Empty)
+            {
+                msgErro += "Campo Veículo em branco.\n";
+            }
+            if (txtDistancia.Text == string.Empty)
+            {
+                msgErro += "Campo Distâcia em branco.\n";
+            }
+            else if (!decimal.TryParse(txtDistancia.Text, out decimal result))
+            {
+                msgErro += "Campo Distância inválido.\n";
+            }
+            if (txtLitros.Text == string.Empty)
+            {
+                msgErro += "Campo Litros em branco.\n";
+            }
+            else if (!decimal.TryParse(txtLitros.Text, out decimal result))
+            {
+                msgErro += "Campo Litros inválido.\n";
+            }
+            return msgErro;
+        }
+    }
+}
